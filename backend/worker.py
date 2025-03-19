4bb3f63ad07de02cdc0b452553e76d1b39ef73e1
Anthony Coutant <anthony.coutant@gmail.com>
2025-03-19 16:06:05 +0100

    Add Celery worker script

---
+++ backend/worker.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+import argparse
+import os
+import sys
+
+from app.core.celery import celery_app
+import app.tasks  # Import pour enregistrer toutes les tâches
+
+def main():
+    """Point d'entrée pour démarrer un worker Celery"""
+    parser = argparse.ArgumentParser(description='Start Celery worker')
+    parser.add_argument('--queue', type=str, default='default',
+                        help='Queue to listen on')
+    parser.add_argument('--concurrency', type=int, default=4,
+                        help='Number of worker processes')
+    parser.add_argument('--loglevel', type=str, default='INFO',
+                        help='Logging level')
+    
+    args = parser.parse_args()
+    
+    print(f"Starting worker for queue: {args.queue}")
+    print(f"Concurrency: {args.concurrency}")
+    
+    # Arguments pour Celery
+    argv = [
+        'worker',
+        f'--queues={args.queue}',
+        f'--concurrency={args.concurrency}',
+        f'--loglevel={args.loglevel}',
+        f'--hostname=worker.{args.queue}@%h',
+    ]
+    
+    # Démarrer le worker
+    celery_app.worker_main(argv)
+
+if __name__ == '__main__':
+    main()
